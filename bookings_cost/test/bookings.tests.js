f5a0693293b44a1a3b0fe7ac117ee84e7f7a19a2
Mxolisi <nyawose57@gmail.com>
2022-04-22 15:44:05 +0200

    adding bookings files

--- bookings_cost/test/bookings.tests.js
+++ bookings_cost/test/bookings.tests.js
@@ -1,1 +1,13 @@
+describe("In bookingCost", () => {
+    it("This booking should cost: R45 * 3 = R135.00 and the function should return that if called with this parameter", () => {
+        assert.deepEqual(bookingCost({check_in_day: 'Monday', duration: 3, cat_name : 'Snowy'}), 135.00);
+    })
 
+    it("This booking should cost: R45 * 3 = R135.00 * 0.75 = 101.25 and the function should return that if called with this parameter", () => {
+        assert.deepEqual(bookingCost({check_in_day: 'Wednesday', duration: 3, cat_name : 'Snowy'}), 101.25);
+    })
+
+    it("This booking should cost: R45 * 3 = R135.00 * 0.85 = 114.75 and the function should return that if called with this parameter", () => {
+        assert.deepEqual(bookingCost({check_in_day: 'Friday', duration: 3, cat_name : 'Snowy'}), 114.75);
+    })
+})
